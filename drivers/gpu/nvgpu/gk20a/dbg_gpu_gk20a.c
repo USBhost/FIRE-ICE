a612666c1fffd3b02a2f29c23414734e9a4b1d56
Terje Bergstrom <tbergstrom@nvidia.com>
2015-01-13 03:17:16 +0200

    gpu: nvgpu: Do not panic if PMU/regops not supported
    Fix panics when using regops when PMU is disabled, or when whitelists
    have not been defined.
    
    Bug 1592505
    
    Change-Id: I316c98147c54be7b1114ad23049ce3a634d4805e
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/671841
    
    Conflicts:
    	drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c

--- drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c
@@ -558,8 +558,9 @@ static int dbg_set_powergate(struct dbg_session_gk20a *dbg_s,
 			if (err)
 				return -EPERM;
 
-			/*do elpg disable before clock gating disable*/
-			gk20a_pmu_disable_elpg(g);
+			/*do elpg disable before clock gating */
+			if (support_gk20a_pmu(g->dev))
+				gk20a_pmu_disable_elpg(g);
 			g->ops.clock_gating.slcg_gr_load_gating_prod(g,
 					false);
 			g->ops.clock_gating.slcg_perf_load_gating_prod(g,
@@ -597,7 +598,8 @@ static int dbg_set_powergate(struct dbg_session_gk20a *dbg_s,
 			g->ops.clock_gating.slcg_perf_load_gating_prod(g,
 					g->slcg_enabled);
 
-			gk20a_pmu_enable_elpg(g);
+			if (support_gk20a_pmu(g->dev))
+				gk20a_pmu_enable_elpg(g);
 
 			gk20a_dbg(gpu_dbg_gpu_dbg | gpu_dbg_fn, "module idle");
 			gk20a_idle(dbg_s->pdev);
