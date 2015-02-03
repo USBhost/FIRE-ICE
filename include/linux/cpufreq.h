33317f0ebf0e4c642c191982ad7842a8c6c1bf53
XileForce <rdaguiar360@gmail.com>
2015-02-03 15:32:37 -0800

    Shamu: Add Wheatley Governor
    Change-Id: I24ebd446630bfcdb8b77709ed0f6a66b2e7cea8c

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -403,6 +403,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_wheatley)
 #endif
 
 
