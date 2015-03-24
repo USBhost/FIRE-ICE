ff497970249da13f9b89810467190b2b689c1e05
Seshendra Gadagottu <sgadagottu@nvidia.com>
2015-03-24 13:24:11 -0700

    tegra: gpu: disable touch boost for gpu
    Gpu boosting with input events is causing more gpu power consumption
    than required. To avoid this, touch boot for gpu is disabled by not
    registering gpu device for cfboost frame work. Current rail gate
    entry/exit latencies are fast enough to give smooth user experience.
    
    Bug 200087243
    
    Change-Id: I18673d9c95a44ce9bee87e860b4edb29212dc766
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/721989
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>
    
    Conflicts:
    	drivers/gpu/nvgpu/gk20a/gk20a.c

--- drivers/gpu/nvgpu/gk20a/gk20a.c
+++ drivers/gpu/nvgpu/gk20a/gk20a.c
@@ -45,7 +45,6 @@
 #include <linux/clk/tegra.h>
 
 #include <linux/sched.h>
-#include <linux/input-cfboost.h>
 
 
 #include "gk20a.h"
@@ -920,13 +919,6 @@ static int gk20a_pm_finalize_poweron(struct device *dev)
 
 	gk20a_scale_resume(pdev);
 
-#ifdef CONFIG_INPUT_CFBOOST
-	if (!g->boost_added) {
-		gk20a_dbg_info("add touch boost");
-		cfb_add_device(dev);
-		g->boost_added = true;
-	}
-#endif
 done:
 	return err;
 }
@@ -1435,11 +1427,6 @@ static int __exit gk20a_remove(struct platform_device *dev)
 {
 	struct gk20a *g = get_gk20a(dev);
 	gk20a_dbg_fn("");
-
-#ifdef CONFIG_INPUT_CFBOOST
-	if (g->boost_added)
-		cfb_remove_device(&dev->dev);
-#endif
 
 	if (g->remove_support)
 		g->remove_support(dev);
