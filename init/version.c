476e3410498a34eebc91c53ce6f074b9070b6d6b
USBhost <josiahspore@gmail.com>
2015-04-04 23:06:56 -0700

    customize version name

--- init/version.c
+++ init/version.c
@@ -39,12 +39,11 @@ struct uts_namespace init_uts_ns = {
 };
 EXPORT_SYMBOL_GPL(init_uts_ns);
 
-/* FIXED STRINGS! Don't touch! */
+/* FIXED STRINGS! Don't touch! *//* Huh... But I Will */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (" LINUX_COMPILE_BY ")"
 	" (" LINUX_COMPILER ") %s\n";
