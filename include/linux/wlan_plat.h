f0144bca842b537675f744d0b15e3f6fc0806f8a
USBhost <josiahspore@gmail.com>
2015-05-15 18:50:48 -0700

    reverted net: wireless: bcmdhd: fix build for flounder
    some one forgot to put "int (*get_wake_irq)(void);"
    in wlan_plat.h (reverted from commit cd3ff2d2e3ca8bffdf2fd8505a819dfe33740706)
    
    and fix build

--- include/linux/wlan_plat.h
+++ include/linux/wlan_plat.h
@@ -28,7 +28,6 @@ struct wifi_platform_data {
 	int (*get_mac_addr)(unsigned char *buf);
 	int (*get_wake_irq)(void);
 	void *(*get_country_code)(char *ccode, u32 flags);
-	int (*get_wake_irq)(void);
 	struct sysedp_consumer *sysedpc;
 };
 
