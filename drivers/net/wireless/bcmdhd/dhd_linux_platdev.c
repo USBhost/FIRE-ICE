9a62e44cb07fed45b99d4aabda75300ba98f7e4e
Insun Song <isong@broadcom.com>
2015-06-12 12:28:55 -0700

    net: wireless: bcmdhd: change pcie enumearion sequence for OOB device
    Change-Id: I941902a4d1842f0a3913a942355df993729407a7
    Signed-off-by: Insun Song <isong@broadcom.com>
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
+++ drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
@@ -542,7 +542,6 @@ static int dhd_wifi_platform_load_pcie(void)
 	if (dhd_wifi_platdata == NULL) {
 		err = dhd_bus_register();
 	} else {
-		if (dhd_download_fw_on_driverload) {
 			/* power up all adapters */
 			for (i = 0; i < dhd_wifi_platdata->num_adapters; i++) {
 				int retry = POWERUP_MAX_RETRY;
@@ -586,13 +585,11 @@ static int dhd_wifi_platform_load_pcie(void)
 					return -ENODEV;
 				}
 			}
-		}
 
 		err = dhd_bus_register();
 
 		if (err) {
 			DHD_ERROR(("%s: pcie_register_driver failed\n", __FUNCTION__));
-			if (dhd_download_fw_on_driverload) {
 				/* power down all adapters */
 				for (i = 0; i < dhd_wifi_platdata->num_adapters; i++) {
 					adapter = &dhd_wifi_platdata->adapters[i];
@@ -600,7 +597,6 @@ static int dhd_wifi_platform_load_pcie(void)
 					wifi_platform_set_power(adapter,
 						FALSE, WIFI_TURNOFF_DELAY);
 				}
-			}
 		}
 	}
 
