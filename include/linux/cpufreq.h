91b44fcbbe6e587c39000594065e732ce0899a5d
Paul Reioux <reioux@gmail.com>
2015-07-19 10:28:36 +0800

    Flounder: Add IntelliActive CPU governor
    Change-Id: I660e5d3830faf5f84761c3cbff2c7eb585263fb6

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -406,6 +406,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 
