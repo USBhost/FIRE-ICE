1f6d343b7fe53d974e57d553a5914dfef3dc7abe
USBhost <josiahspore@gmail.com>
2016-01-08 16:13:42 -0800

    arm/arm64: flounder: add missing cpu frequencies (1/2) also set cpu speed to 2.5

--- arch/arm/mach-tegra/board-flounder-sensors.c
+++ arch/arm/mach-tegra/board-flounder-sensors.c
@@ -454,7 +454,13 @@ static struct throttle_table cpu_throttle_table[] = {
 	/* CPU_THROT_LOW cannot be used by other than CPU */
 	/*      CPU,    GPU,  C2BUS,  C3BUS,   SCLK,    EMC   */
 	{ { 2499000, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
+	{ { 2473500, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
+	{ { 2448000, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
+	{ { 2422500, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
 	{ { 2397000, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
+	{ { 2371500, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
+	{ { 2346000, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
+	{ { 2320500, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
 	{ { 2295000, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
 	{ { 2269500, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
 	{ { 2244000, NO_CAP, NO_CAP, NO_CAP, NO_CAP, NO_CAP } },
@@ -548,6 +554,14 @@ static struct balanced_throttle cpu_throttle = {
 static struct throttle_table gpu_throttle_table[] = {
 	/* CPU_THROT_LOW cannot be used by other than CPU */
 	/*      CPU,    GPU,  C2BUS,  C3BUS,   SCLK,    EMC   */
+	{ { 2499000, 867600, 480000, 756000, 384000, 924000 } },
+	{ { 2473500, 857000, 480000, 756000, 384000, 924000 } },
+	{ { 2448000, 846400, 480000, 756000, 384000, 924000 } },
+	{ { 2422500, 835800, 480000, 756000, 384000, 924000 } },
+	{ { 2397000, 825200, 480000, 756000, 384000, 924000 } },
+	{ { 2371500, 814600, 480000, 756000, 384000, 924000 } },
+	{ { 2346000, 804000, 480000, 756000, 384000, 924000 } },
+	{ { 2320500, 793400, 480000, 756000, 384000, 924000 } },
 	{ { 2295000, 782800, 480000, 756000, 384000, 924000 } },
 	{ { 2269500, 772200, 480000, 756000, 384000, 924000 } },
 	{ { 2244000, 761600, 480000, 756000, 384000, 924000 } },
