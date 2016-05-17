3920d291133274c69feddc38d13e8ba09130fcae
Vartom <babanakov_dima@mail.ru>
2016-05-17 21:54:24 +0600

    fix code

--- arch/arm64/mach-tegra/denver-knobs.c
+++ arch/arm64/mach-tegra/denver-knobs.c
@@ -465,9 +465,9 @@ static int __init denver_pmic_init(void)
 		pr_err("%s: invalid lock setting [0|1]: read %d\n", __func__, lock);
 		goto done;
 	}
-
-    if (fuse_cp_rev_check() >= 1)
+	if (fuse_cp_rev_check() >= 1) {
         voltage = 16;
+	}
 
 	err = denver_set_pmic_config(type, (u16)voltage, lock);
 
