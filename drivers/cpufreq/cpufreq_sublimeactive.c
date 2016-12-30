56979086952587ef179d2675a61dff98caf7e69d
Dela Anthonio <dell.anthonio@gmail.com>
2016-12-30 13:37:08 -0500

    cpufreq/sa: Adjust cpufreq relations
    Previously, thr relations were set so that middle frequencies would be favored. For example, if increasing the frequency, the current and max frequencies would be averaged and rounded downwards to the nearest frequency. Likewise when reducing the frequency, The current and minimum frequencies are averaged and rounded to the next highest frequency. I see this as being useful only if the boundaries can be violated. However __cpufreq_driver_target() already does a boundary checks. Making this change will make scaling the cpu frequency more responsive.

--- drivers/cpufreq/cpufreq_sublimeactive.c
+++ drivers/cpufreq/cpufreq_sublimeactive.c
@@ -64,19 +64,23 @@ static void sa_def_check_cpu(int cpu, unsigned int load)
 
 	/* Check for frequency decrease */
 	if (load < sa_tuners->down_threshold) {
-		if (input_event_boost(sa_tuners->touchboost_dur))
+		if (input_event_boost(sa_tuners->touchboost_dur)) {
 			freq_target = sa_tuners->touchboost_min_freq;
-		else
+			__cpufreq_driver_target(policy, freq_target,
+						CPUFREQ_RELATION_H);
+		} else {
 			freq_target = (policy->cur + policy->min) / 2;
-		__cpufreq_driver_target(policy, freq_target,
-					CPUFREQ_RELATION_L);
+			__cpufreq_driver_target(policy, freq_target,
+						CPUFREQ_RELATION_H);
+		}
+
 	}
 
 	/* Check for frequency increase */
 	else if (load >= max(sa_tuners->up_threshold, prev_load)) {
 		freq_target = (policy->max + policy->cur) / 2;
 		__cpufreq_driver_target(policy, freq_target,
-					 CPUFREQ_RELATION_H);
+					 CPUFREQ_RELATION_L);
 	}
 
 }
