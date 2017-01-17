420e75934c581ac923b2eb6ab38209bb07a594ad
Deepak Nibade <dnibade@nvidia.com>
2017-01-17 11:30:12 +0530

    gpu: nvgpu: serialize debug session IOCTLs
    Hold debug_s->ioctl_lock for all debug session
    IOCTLs to prevent multi-threaded user space
    IOCTL calls
    
    debug session IOCTL calls are not thread-safe
    and hence this serialization is required
    
    NvBug 1832267
    Bug: 32370135
    
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    (cherry picked from commit 72f27f77472503ccaa840c1fc01efbf5df6075d0)
    Signed-off-by: Xia Yang <xiay@nvidia.com>
    Change-Id: I847ac951601d4f0093546b592bdb8c8f00185317
    (cherry picked from commit 9ffc3dd0659cc276f0da5d11c705d93dbeea16e4)

--- drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/dbg_gpu_gk20a.c
@@ -1,7 +1,7 @@
 /*
  * Tegra GK20A GPU Debugger/Profiler Driver
  *
- * Copyright (c) 2013-2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2013-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -91,6 +91,7 @@ int gk20a_dbg_gpu_do_dev_open(struct inode *inode, struct file *filp, bool is_pr
 
 	INIT_LIST_HEAD(&dbg_session->dbg_s_list_node);
 	init_waitqueue_head(&dbg_session->dbg_events.wait_queue);
+	mutex_init(&dbg_session->ioctl_lock);
 	dbg_session->dbg_events.events_enabled = false;
 	dbg_session->dbg_events.num_pending_events = 0;
 
@@ -389,6 +390,9 @@ long gk20a_dbg_gpu_dev_ioctl(struct file *filp, unsigned int cmd,
 		gk20a_idle(g->dev);
 	}
 
+	/* protect from threaded user space calls */
+	mutex_lock(&dbg_s->ioctl_lock);
+
 	switch (cmd) {
 	case NVHOST_DBG_GPU_IOCTL_BIND_CHANNEL:
 		err = dbg_bind_channel_gk20a(dbg_s,
@@ -425,6 +429,8 @@ long gk20a_dbg_gpu_dev_ioctl(struct file *filp, unsigned int cmd,
 		err = -ENOTTY;
 		break;
 	}
+
+	mutex_unlock(&dbg_s->ioctl_lock);
 
 	if ((err == 0) && (_IOC_DIR(cmd) & _IOC_READ))
 		err = copy_to_user((void __user *)arg,
