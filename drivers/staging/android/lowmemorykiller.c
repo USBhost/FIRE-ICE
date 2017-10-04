a913e880093590412bbd6b0a242641931d34cb47
USBhost <josiahspore@gmail.com>
2017-10-03 20:03:37 -0700

    Revert "lowmemorykiller: account for unevictable pages"
    This reverts commit d94337be234ac0b38a47150cf376fc1522032740.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -105,9 +105,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES)
 						- totalreserve_pages;
-	int other_file = global_page_state(NR_FILE_PAGES)
-						- global_page_state(NR_SHMEM)
-						- global_page_state(NR_UNEVICTABLE)
+	int other_file = global_page_state(NR_FILE_PAGES) -
+						global_page_state(NR_SHMEM)
 						- total_swapcache_pages();
 	si_swapinfo(&swap_info);
 	/* basically this is ~50% until killing starts */
